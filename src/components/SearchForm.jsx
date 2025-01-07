e47c858a8aa7ae2dbffc6d5ae746304c42e3f252
Micheal <michealobarewon2015@gmail.com>
2025-01-07 23:00:36 +0100

    search form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -2,10 +2,21 @@ import { Form } from 'react-router';
 
 const SearchForm = () => {
   return (
-    <div>
+    <div className="mx-auto px-8 py-4">
       <Form>
-        <input type="search" name="search" />
-        <button type="submit">Search</button>
+        <div className="flex gap-1">
+          <input
+            className="bg-slate-400  border-solid border-transparent  border-1 rounded-sm"
+            type="search"
+            name="search"
+          />
+          <button
+            className="bg-slate-800 text-white rounded-sm text-[0.875rem] px-[0.4rem] py-1"
+            type="submit"
+          >
+            Search
+          </button>
+        </div>
       </Form>
     </div>
   );
