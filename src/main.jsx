8954760c6ff41803b47737d8e84b44525b2e0188
Micheal <michealobarewon2015@gmail.com>
2025-01-09 22:28:05 +0100

    reux added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-const queryClient = new QueryClient();
+import { Provider } from 'react-redux';
+import store from './store.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
+  <Provider store={store}>
     <App />
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
+  </Provider>
 );
