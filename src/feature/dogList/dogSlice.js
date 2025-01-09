19f9df02db59a9d450a7a48420dda1e8137c496a
Micheal <michealobarewon2015@gmail.com>
2025-01-09 23:45:35 +0100

    asyncthuk added

--- src/feature/dogList/dogSlice.js
+++ src/feature/dogList/dogSlice.js
@@ -1,13 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = `https://api.thedogapi.com/v1/images/search?limit=10&has_breeds=1&api_key=${
+  import.meta.env.VITE_API_KEY
+}`;
 
 const initialState = {
   dogList: [],
   isLoading: true,
 };
 
+export const getDogList = createAsyncThunk(
+  'dog/getDogList',
+  async (dog, thunkAPI) => {
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong');
+    }
+  }
+);
 const dogSlice = createSlice({
   name: 'dog',
   initialState,
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(getDogList.isLoading, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getDogList.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.dogList = action.payload;
+      })
+      .addCase(getDogList.rejected, (state, action) => {
+        console.log(action);
+        state.isLoading = false;
+      });
+  },
 });
 
 export default dogSlice.reducer;
