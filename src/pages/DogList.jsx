538968ed4e5a40671fcb65771fae78966c530515
Micheal <michealobarewon2015@gmail.com>
2025-01-12 23:41:23 +0100

    card fixed for mibile

--- src/pages/DogList.jsx
+++ src/pages/DogList.jsx
@@ -6,30 +6,32 @@ const DogList = () => {
   const { dogList: data } = useSelector((store) => store.dog);
   return (
     <>
-      <section className="mx-auto  px-8 gap-y-8 grid">
+      <section className="mx-auto  px-6 gap-y-8 grid">
         {data.map((dog) => {
           const { bred_for, breed_group, height, name, temperament, weight } =
             dog.breeds[0];
           return (
             <article
-              className=" flex shadow-md items-center rounded-lg w-full "
+              className=" flex shadow-md h-36 justify-between items-center  gap-2 rounded-2xl w-full "
               key={dog.id}
             >
-              <img
-                className="block w-full h-56 object-cover rounded-lg"
-                src={dog.url}
-                alt="dog picture"
-              />
+              <div className=" w-5/12 rounded-2xl h-full">
+                <img
+                  className="block w-full h-full object-cover rounded-2xl"
+                  src={dog.url}
+                  alt="dog picture"
+                />
+              </div>
 
-              <div className=" flex flex-col w-full m-4 p-4 gap-4">
+              <div className=" w-7/12 flex flex-col  p-2 gap-4">
                 <div className=" flex items-center justify-between w-full">
-                  <h3 className="text-xl font-sans font-bold"> {name}</h3>
+                  <h3 className="text-lg font-sans font-bold"> {name}</h3>
                   <span className="block">
                     <FaRegHeart className="text-red-500" />
                   </span>
                 </div>
                 <p className="text-sm font-semibold">
-                  <span className=" font-semibold text-red-500 text-md">
+                  <span className=" font-semibold text-red-500 text-sm">
                     Bred for :{' '}
                   </span>{' '}
                   {bred_for}{' '}
