362b9bdeefb5e6616f45f9ca6bc1d5d07194c32f
Micheal <michealobarewon2015@gmail.com>
2025-01-13 20:14:42 +0100

    minor commit

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,4 +1,5 @@
 import { Form } from 'react-router';
+import { FaSearch } from 'react-icons/fa';
 
 const SearchForm = () => {
   return (
@@ -6,10 +7,10 @@ const SearchForm = () => {
       <Form>
         <div className="flex w-full">
           <input
-            className="w-full px-4 py-2 text-sm font-semibold border-transparent border-solid rounded-lg bg-slate-200 h-9 md:w-2/3 md:rounded-none md:rounded-tl-md md:rounded-bl-md lg:w-4/12 lg:rounded-none lg:rounded-tl-md lg:rounded-bl-md"
+            className="absolute w-full px-4 py-2 text-sm font-semibold border-transparent border-solid rounded-lg bg-slate-200 h-9 md:w-2/3 md:rounded-none md:rounded-tl-md md:rounded-bl-md lg:w-4/12 lg:rounded-none lg:rounded-tl-md lg:rounded-bl-md"
             type="search"
             name="search"
-            placeholder="Search for a dog Breed"
+            placeholder={<FaSearch />}
           />
           <button
             className="bg-slate-600 hidden md:block lg:block text-white font-semibold rounded-se-md rounded-ee-md text-[0.875rem] px-[0.4rem] py-1"
